8c7bcf23080eb6144c37d799eb54a7634eba4e31
mirrorinf <mirrorinf@icloud.com>
2018-10-12 21:54:31 +0800

    Initial Commit

---
+++ Huanffman/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Huanffman
+//
+//  Created by ChuXiaoMin on 2018/10/12.
+//  Copyright © 2018 ChuXiaoMin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
