01d6bc61a50180b823d1380ad63f0ea0e7d0c19c
mirrorinf <mirrorinf@icloud.com>
2018-10-13 19:19:08 +0800

    buggy swift

--- Huanffhuan/Tree.swift
+++ Huanffhuan/Tree.swift
@@ -17,8 +17,24 @@ protocol TreeLike {
     func content(of: Node) -> Stored?
 }
 
+protocol Environment {
+    associatedtype Element
+    func process(_: Element)
+}
+
 extension TreeLike {
-    func po() {
-        
+    fileprivate func __preOrdered<T: Environment>(context: T, node n: Node?) where T.Element == Self.Stored {
+        guard let current = n else {
+            return
+        }
+        if let stored = self.content(of: current) {
+            context.process(stored)
+        }
+        __preOrdered(context: context, node: self.leftChild(of: current))
+        __preOrdered(context: context, node: self.rightChild(of: current))
+    }
+    
+    func preOrdered<T: Environment>(on context: T) {
+        __preOrdered(context: context, node: self.root)
     }
 }
